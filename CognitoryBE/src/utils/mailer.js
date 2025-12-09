9e2b620425ba4c77f600b37afa51b37e6eb4062c
Dev Gupta <devgupta851900@gmail.com>
2025-12-09 10:57:13 +0530

    bumped congitory versions

--- CognitoryBE/src/utils/mailer.js
+++ CognitoryBE/src/utils/mailer.js
@@ -2,23 +2,26 @@ import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 dotenv.config();
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465, 
-  secure: true, 
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
-  },
-});
+// const transporter = nodemailer.createTransport({
+//   host: "smtp.gmail.com",
+//   port: 465, 
+//   secure: true, 
+//   auth: {
+//     user: process.env.SMTP_USER,
+//     pass: process.env.SMTP_PASS,
+//   },
+// });
 
 export const sendMail = async ({ to, subject, html }) => {
-  const mailOptions = {
-    from: `"Cognitory" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    html,
-  };
+  // const mailOptions = {
+  //   from: `"Cognitory" <${process.env.EMAIL_USER}>`,
+  //   to,
+  //   subject,
+  //   html,
+  // };
 
-  await transporter.sendMail(mailOptions);
+  // await transporter.sendMail(mailOptions);
+
+
+  console.log("Mail sent")
 };
